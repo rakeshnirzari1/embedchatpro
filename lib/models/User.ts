641e86ab81e646831bdef9fecd5923172a1c5462
rakeshnirzari1 <rvpnrp@gmail.com>
2025-12-06 11:26:41 +1100

    Fix password hashing issue - prevent double hashing in user creation

--- lib/models/User.ts
+++ lib/models/User.ts
@@ -47,6 +47,11 @@ const UserSchema = new Schema<IUser>({
 UserSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   
+  // Check if password is already hashed (bcrypt hashes start with $2a$, $2b$, or $2y$)
+  if (this.password && this.password.startsWith('$2')) {
+    return next();
+  }
+  
   try {
     const salt = await bcrypt.genSalt(12);
     this.password = await bcrypt.hash(this.password, salt);
